2b8358d037588efab2a3c0f3f5155993478b2c37
z919191 <z919191@naver.com>
2019-07-05 16:12:48 +0900

    한글 데이터 추가

--- B1_ScanningString.java
+++ B1_ScanningString.java
@@ -3,13 +3,13 @@ import java.util.Scanner;
 public class B1_ScanningString {
 
 	public static void main(String[] args) {
-		String source = "1 3 5 7";
+		String source = "홍 길 동";
 		// 주석 추가
 		Scanner sc = new Scanner(source);
 		
-		int num1 = sc.nextInt();
-		int num2 = sc.nextInt();
-		int num3 = sc.nextInt();
+		int num1 = sc.next();
+		int num2 = sc.next();
+		int num3 = sc.next();
 		
 		int sum = num1 + num2 + num3;
 		System.out.printf("%d + %d + %d = %d \n", num1, num2, num3, sum);
